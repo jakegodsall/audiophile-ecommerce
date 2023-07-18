0fdaff4916af9f5c66cdfba051ae094fb2558b57
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-18 18:58:39 +0100

    added config and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'primary-orange': 'var(--color-primary-orange)',
+				'secondary-orange': 'var(--color-secondary-orange)',
+				'primary-black': 'var(--color-primary-black)',
+				'primary-grey': 'var(--color-primary-grey)',
+				'secondary-grey': 'var(--color-secondary-grey)',
+			},
+		},
+	},
+	plugins: [],
+};
