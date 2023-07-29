f512b5cb3dccbd8cd8375f984db048aa27c1f615
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-29 16:40:09 +0100

    Header complete

--- audiophile-frontend/src/app/components/Header.js
+++ audiophile-frontend/src/app/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
 	};
 
 	return (
-		<header className="flex items-center justify-between px-[2.4rem] py-[3.2rem] sm:p-[4rem] ">
+		<header className="mx-[2.4rem] flex items-center justify-between border-b-[1px] border-white/10 py-[3.2rem] sm:mx-[4rem] sm:py-[4rem]">
 			<div className="flex items-center justify-between gap-x-[7.5rem] sm:gap-[4.2rem] ">
 				<div className="md:hidden">
 					<Hamburger
@@ -31,24 +31,28 @@ const Header = () => {
 					className="h-[2.5rem] w-[14.3rem]"
 					alt="audiophile logo"
 				/>
-				<nav className="w-[0]  overflow-hidden transition-all duration-1000 md:w-[45rem]">
-					<ul className="flex gap-[3.4rem]">
-						<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
-							home
-						</li>
-						<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
-							headphones
-						</li>
-						<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
-							speakers
-						</li>
-						<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
-							earphones
-						</li>
-					</ul>
-				</nav>
 			</div>
-			<Image src={CartIcon} alt="shopping cart" />
+			<nav className="w-[0]  overflow-hidden transition-all duration-1000 md:w-[45rem]">
+				<ul className="flex gap-[3.4rem]">
+					<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
+						home
+					</li>
+					<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
+						headphones
+					</li>
+					<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
+						speakers
+					</li>
+					<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
+						earphones
+					</li>
+				</ul>
+			</nav>
+			<Image
+				src={CartIcon}
+				alt="shopping cart"
+				className="cursor-pointer"
+			/>
 		</header>
 	);
 };
