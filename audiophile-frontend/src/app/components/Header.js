879fddb2dd780a648ce98f1342a23215e6ad4131
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-30 19:31:30 +0100

    fixed position of logo in Header

--- audiophile-frontend/src/app/components/Header.js
+++ audiophile-frontend/src/app/components/Header.js
@@ -16,23 +16,22 @@ const Header = () => {
 	};
 
 	return (
-		<header className="mx-[2.4rem] flex items-center justify-between border-b-[1px] border-white/10 py-[3.2rem] sm:mx-[4rem] sm:py-[4rem]">
-			<div className="flex items-center justify-between gap-x-[7.5rem] sm:gap-[4.2rem] ">
-				<div className="md:hidden">
-					<Hamburger
-						isActive={mobileMenuOpen}
-						setIsActive={setIsActiveHandler}
-					/>
-				</div>
-				<Image
-					src={Logo}
-					width={143}
-					height={23}
-					className="h-[2.5rem] w-[14.3rem]"
-					alt="audiophile logo"
+		<header className="mx-[2.4rem] flex items-center justify-between border-b-[1px] border-white/10 py-[3.2rem] sm:mx-[4rem] sm:justify-start sm:py-[4rem] md:justify-between">
+			<div className="md:hidden">
+				<Hamburger
+					isActive={mobileMenuOpen}
+					setIsActive={setIsActiveHandler}
 				/>
 			</div>
-			<nav className="w-[0]  overflow-hidden transition-all duration-1000 md:w-[45rem]">
+			<Image
+				src={Logo}
+				width={143}
+				height={23}
+				className="h-[2.5rem] w-[14.3rem] sm:ml-[4.2rem] md:ml-0"
+				alt="audiophile logo"
+			/>
+
+			<nav className="hidden w-[0] md:flex md:w-[45rem]">
 				<ul className="flex gap-[3.4rem]">
 					<li className="letter-spacing-[2px] cursor-pointer text-[1.3rem] uppercase tracking-[2px] text-white hover:text-primary-orange">
 						home
@@ -51,7 +50,7 @@ const Header = () => {
 			<Image
 				src={CartIcon}
 				alt="shopping cart"
-				className="cursor-pointer"
+				className="cursor-pointer sm:ml-auto md:ml-0"
 			/>
 		</header>
 	);
