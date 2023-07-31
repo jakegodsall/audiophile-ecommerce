08ef34ce65479a8cccc27b304d61db9a80fd4746
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-31 19:43:31 +0100

    Built all viewports of BringingAudio component

--- audiophile-frontend/src/app/components/BringingAudio.js
+++ audiophile-frontend/src/app/components/BringingAudio.js
@@ -1,24 +1,28 @@
-import Image from 'next/image';
-
-import MobileImage from '../../../public/images/shared/mobile/image-best-gear.jpg';
-import TabletImage from '../../../public/images/shared/tablet/image-best-gear.jpg';
-import DesktopImage from '../../../public/images/shared/desktop/image-best-gear.jpg';
-
 const BringingAudio = () => {
 	return (
-		<div className="flex flex-col items-center bg-white px-[2.4rem] py-[4rem]">
-			<Image
-				src={MobileImage}
-				width="327"
-				height="300"
-				className="rounded-2xl"
-			/>
-			<div className="my-[4.2rem] flex flex-col items-center gap-[3.2rem]">
-				<h3 className="text-center text-[2.8rem] font-bold uppercase tracking-[1px] ">
+		<div className="flex flex-col items-center bg-white px-[2.4rem] py-[4rem] lg:flex-row-reverse lg:justify-center lg:gap-[12.5rem] lg:px-[4rem] lg:py-[16rem]">
+			<picture>
+				<source
+					media="(min-width: 1024px)"
+					srcSet="/images/shared/desktop/image-best-gear.jpg"
+				/>
+				<source
+					media="(min-width: 640px)"
+					srcSet="/images/shared/tablet/image-best-gear.jpg"
+				/>
+				<img
+					src="/images/shared/mobile/image-best-gear.jpg"
+					alt="man with headphones"
+					className="w-full rounded-2xl sm:max-w-[75rem] lg:max-w-[58rem]"
+				/>
+			</picture>
+
+			<div className="my-[4.2rem] flex flex-col items-center gap-[3.2rem] sm:max-w-[57rem] lg:max-w-[44.5rem] ">
+				<h3 className="text-center text-[2.8rem] font-bold uppercase tracking-[0.1rem] sm:text-[4rem] sm:leading-[4.4rem] sm:tracking-[0.15rem] lg:text-left">
 					Bringing you the{' '}
 					<span className="text-primary-orange">best</span> audio gear
 				</h3>
-				<p className="text-center text-[1.5rem] leading-[2.5rem] text-black/50">
+				<p className="text-center text-[1.5rem] leading-[2.5rem] text-black/50 lg:text-left">
 					Located at the heart of New York City, Audiophile is the
 					premier store for high end headphones, earphones, speakers,
 					and audio accessories. We have a large showroom and luxury
