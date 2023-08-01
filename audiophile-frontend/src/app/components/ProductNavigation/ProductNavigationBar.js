0ec11806638407d32023704ce51a5e101e7c467e
jakegodsall <jake.edward.godsall@gmail.com>
2023-08-01 16:59:05 +0100

    Added all layouts to ProductNavigationBar

--- audiophile-frontend/src/app/components/ProductNavigation/ProductNavigationBar.js
+++ audiophile-frontend/src/app/components/ProductNavigation/ProductNavigationBar.js
@@ -1,9 +1,45 @@
 import ProductNavigationItem from './ProductNavigationItem';
 
+import HeadphoneImage from '../../../../public/images/shared/desktop/image-category-thumbnail-headphones.png';
+import SpeakersImage from '../../../../public/images/shared/desktop/image-category-thumbnail-speakers.png';
+import EarphonesImage from '../../../../public/images/shared/desktop/image-category-thumbnail-earphones.png';
+
+const PRODUCTS = [
+	{
+		id: 1,
+		title: 'headphones',
+		image: HeadphoneImage,
+		link: '#/',
+	},
+	{
+		id: 2,
+		title: 'speakers',
+		image: SpeakersImage,
+		link: '#/',
+	},
+	{
+		id: 3,
+		title: 'earphones',
+		image: EarphonesImage,
+		link: '#/',
+	},
+];
+
 const ProductNavigationBar = () => {
 	return (
-		<div className="flex w-full flex-col items-center bg-white py-[4rem]">
-			<ProductNavigationItem />
+		<div className="flex w-full flex-col items-center bg-white px-[2.4rem] py-[4rem]">
+			<ul className="flex w-full flex-col items-center gap-[6.8rem] sm:max-w-[75rem] sm:flex-row sm:gap-[1rem] lg:max-w-[111rem] lg:gap-[3rem]">
+				{PRODUCTS.map((product) => {
+					return (
+						<li
+							key={product.id}
+							className="flex w-full flex-col items-center"
+						>
+							<ProductNavigationItem product={product} />
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 };
--- audiophile-frontend/src/app/components/ProductNavigation/ProductNavigationItem.js
+++ audiophile-frontend/src/app/components/ProductNavigation/ProductNavigationItem.js
@@ -1,21 +1,20 @@
 import Image from 'next/image';
 
-import HeadphonesImage from '../../../../public/images/shared/desktop/image-category-thumbnail-headphones.png';
 import IconArrowRight from '../../../../public/images/shared/desktop/icon-arrow-right.svg';
 
-const ProductNavigationItem = () => {
+const ProductNavigationItem = ({ product }) => {
 	return (
-		<div className="relative flex w-full max-w-[32.7rem] flex-col">
+		<div className="relative flex w-full max-w-[32.7rem] flex-col lg:max-w-[35rem] ">
 			<Image
-				src={HeadphonesImage}
+				src={product.image}
 				alt="headphones"
-				className="absolute left-0 right-0 top-[-4rem] mx-auto h-[10.4rem] w-auto"
+				className="absolute left-0 right-0 top-[-4rem] mx-auto h-[10.4rem] w-auto lg:h-auto lg:w-[15rem]"
 			/>
-			<div className="flex h-[14.5rem] w-full max-w-[32.7rem] flex-col items-center gap-[1.7rem] rounded-xl bg-[#F1F1F1] pb-[2.2rem] pt-[6.8rem]">
-				<p className="text-[1.5rem] font-bold uppercase tracking-[0.18rem]">
-					Headphones
+			<div className="flex h-[14.5rem] w-full max-w-[32.7rem] flex-col items-center gap-[1.7rem] rounded-xl bg-[#F1F1F1] pb-[2.2rem] pt-[6.8rem] lg:h-[20.4rem] lg:max-w-[35rem] lg:pt-[10rem]">
+				<p className="cursor-default text-[1.5rem] font-bold uppercase tracking-[0.18rem] lg:text-[1.8rem]">
+					{product.title}
 				</p>
-				<a href="#/" class="flex items-center gap-4">
+				<a href={product.link} class="flex items-center gap-4">
 					<p className="text-[1.3rem] font-bold uppercase tracking-[0.1rem] opacity-50">
 						shop
 					</p>
