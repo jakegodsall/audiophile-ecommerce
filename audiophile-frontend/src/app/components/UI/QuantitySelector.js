a202d1512004988280d172582a824f0b5ff47a14
jakegodsall <jake.edward.godsall@gmail.com>
2023-08-04 20:34:12 +0100

    Added QuantitySelector component for adding products to cart

---
+++ audiophile-frontend/src/app/components/UI/QuantitySelector.js
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+
+const QuantitySelector = () => {
+	const [quantity, setQuantity] = useState(1);
+
+	const decreaseHandler = () => {
+		if (quantity > 0) {
+			setQuantity((prevValue) => (prevValue -= 1));
+		}
+	};
+
+	const increaseHandler = () => {
+		setQuantity((prevValue) => (prevValue += 1));
+	};
+
+	return (
+		<div className="flex gap-[2.5rem] bg-[#F1F1F1] p-[1.5rem]">
+			<button
+				className="text-[1.5rem] font-bold tracking-[0.1rem] text-black/25"
+				onClick={decreaseHandler}
+				type="button"
+			>
+				-
+			</button>
+			<p className="text-[1.5rem] font-bold tracking-[0.1rem] text-black">
+				{quantity}
+			</p>
+			<button
+				className="text-[1.5rem] font-bold tracking-[0.1rem] text-black/25"
+				onClick={increaseHandler}
+				type="button"
+			>
+				+
+			</button>
+		</div>
+	);
+};
+
+export default QuantitySelector;
