6878775d384496716ba5f2f76fb199f4bc006994
jakegodsall <jake.edward.godsall@gmail.com>
2023-08-06 20:23:31 +0100

    Created the Hero component

---
+++ src/app/components/Hero.js
@@ -0,0 +1,15 @@
+const Hero = () => {
+	return (
+		<div className="flex flex-col items-center">
+			<p>new product</p>
+			<p>XX99 Mark II Headphones</p>
+			<p>
+				Experience natural, lifelike audio and exceptional build quality
+				made for the passionate music enthusiast.
+			</p>
+			<button type="button">see product</button>
+		</div>
+	);
+};
+
+export default Hero;
