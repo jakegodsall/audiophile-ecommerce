ce31a17aadb719343bfbf2aa54bfa74b87e4dbe0
jakegodsall <jake.edward.godsall@gmail.com>
2023-08-10 18:50:35 +0100

    Added desktop layout to HomePageProductSection1

--- src/app/components/HomePageProductSections/HomePageProductSection1.jsx
+++ src/app/components/HomePageProductSections/HomePageProductSection1.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const HomePageProductSection1 = () => {
 	return (
-		<div className="mx-[2.4rem] flex w-full flex-col items-center overflow-hidden rounded-[0.8rem] bg-primary-orange px-[2.3rem] sm:max-w-[75rem] lg:max-w-none lg:flex-row">
-			<div className="relative lg:flex-1">
+		<div className="flex w-full flex-col items-center overflow-hidden rounded-[0.8rem] bg-primary-orange px-[2.3rem] sm:max-w-[75rem] lg:max-w-none lg:flex-row">
+			<div className="relative">
 				<picture>
 					<source
 						media="(min-width: 1024px)"
@@ -16,21 +16,21 @@ const HomePageProductSection1 = () => {
 					<img
 						src="/images/home/mobile/image-speaker-zx9.png"
 						alt="ZX0 Speaker"
-						className="mb-[3.2rem] mt-[5.5rem] w-[17.2rem]"
+						className="mb-[3.2rem] mt-[5.5rem] max-w-[17.2rem] lg:relative lg:bottom-[-4rem] lg:mx-[11.8rem] lg:max-w-[410rem]"
 					/>
 				</picture>
 			</div>
-			<div className="flex max-w-[28rem] flex-col items-center lg:flex-1">
-				<p className="mb-[2.4rem] max-w-[18rem] text-center text-[3.6rem] uppercase leading-[4rem] leading-[5.6rem] tracking-[0.129rem] text-white sm:max-w-[26rem] sm:text-[5.6rem]">
+			<div className="flex max-w-[28rem] flex-col items-center lg:items-start">
+				<p className="mb-[2.4rem] max-w-[18rem] text-center text-[3.6rem] uppercase leading-[4rem] tracking-[0.129rem] text-white sm:max-w-[26rem] sm:text-[5.6rem] sm:leading-[5.6rem] lg:text-left">
 					ZX9 Speaker
 				</p>
-				<p className="mb-[2.4rem] text-center text-[1.5rem] leading-[2.5rem] text-white/75">
+				<p className="mb-[2.4rem] text-center text-[1.5rem] leading-[2.5rem] text-white/75 lg:mb-[4rem] lg:text-left">
 					Upgrade to premium speakers that are phenomenally built to
 					deliver truly remarkable sound.
 				</p>
 				<Link
 					href="speakers/zx7-speaker"
-					className="relative z-20 mb-[5.5rem] bg-black px-[3rem] py-[1.5rem] text-center text-[1.3rem] uppercase tracking-[0.1rem] text-white"
+					className="relative z-20 mb-[5.5rem] bg-black px-[3rem] py-[1.5rem] text-center text-[1.3rem] uppercase tracking-[0.1rem] text-white lg:mb-0"
 				>
 					See Product
 				</Link>
