cc9d36a9fee72120c702c6738ba41c583b363775
jakegodsall <jake.edward.godsall@gmail.com>
2023-09-20 15:15:40 +0200

    Fixed homepage layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import BringingAudio from './components/BringingAudio';
 import Hero from './components/Hero';
 import HomePageProductSection1 from './components/HomePageProductSections/HomePageProductSection1';
 import HomePageProductSection2 from './components/HomePageProductSections/HomePageProductSection2';
@@ -8,10 +9,11 @@ export default function Home() {
 		<div className="flex flex-col items-center">
 			<Hero />
 			<ProductNavigationBar />
-			<div className="mx-[2.4rem] mt-[9rem] flex w-full flex-col items-center gap-[2.4rem] sm:mt-[6rem] lg:mx-0 lg:max-w-[111rem] lg:gap-[4.8rem]">
+			<div className="mb-[4rem] flex w-full max-w-[37.8em] flex-col items-center  gap-[2.4rem] px-[2.4rem] sm:mt-[6rem] sm:max-w-none lg:mx-0 lg:mx-0 lg:max-w-[111rem] lg:gap-[4.8rem] lg:px-0">
 				<HomePageProductSection1 />
 				<HomePageProductSection2 />
 			</div>
+			<BringingAudio />
 		</div>
 	);
 }
