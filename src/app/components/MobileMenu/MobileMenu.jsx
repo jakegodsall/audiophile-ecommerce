9d03e14178501e4fd5ebd1df474672fd3b58394d
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-07 12:26:15 +0100

    addded element to make mobile menu fit full screen

--- src/app/components/MobileMenu/MobileMenu.jsx
+++ src/app/components/MobileMenu/MobileMenu.jsx
@@ -8,18 +8,26 @@ const MobileMenu = ({ closeMenu }) => {
 	};
 
 	return (
-		<motion.div
-			initial={{ y: -1000 }}
-			animate={{ y: 20 }}
-			exit={{ y: -1000 }}
-			transition={{ duration: 1 }}
-			className="absolute bottom-0 left-0 right-0 top-[-2rem] z-20 h-full bg-black/40"
-			onClick={closeMobileMenu}
-		>
-			<div className="fixed left-0 top-[9rem] w-full">
-				<ProductNavigationBar />
-			</div>
-		</motion.div>
+		<>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1, transition: { delay: 0.6 } }}
+				exit={{ opacity: 0 }}
+				className="absolute bottom-0 left-0 right-0 top-[9rem] h-screen bg-white"
+			></motion.div>
+			<motion.div
+				initial={{ y: -1000 }}
+				animate={{ y: 20 }}
+				exit={{ y: -1000 }}
+				transition={{ duration: 1 }}
+				className="absolute bottom-0 left-0 right-0 top-[-2rem] z-20 h-full bg-black/40"
+				onClick={closeMobileMenu}
+			>
+				<div className="fixed left-0 top-[9rem] w-full">
+					<ProductNavigationBar />
+				</div>
+			</motion.div>
+		</>
 	);
 };
 
